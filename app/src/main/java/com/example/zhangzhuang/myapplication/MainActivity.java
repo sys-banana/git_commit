090e932afbf25767035d22d59cae706628ce49a4
zhangzhuang <1013376821@qq.com>
2019-06-28 10:21:44 +0800

    number one

---
+++ app/src/main/java/com/example/zhangzhuang/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.zhangzhuang.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        String url = "http://wwww.baidu.com";
+//        OkHttpClient okHttpClient = new OkHttpClient();
+//        final Request request = new Request.Builder()
+//                .url(url)
+//                .get()
+//                .build();
+//        Call call = okHttpClient.newCall(request);
+
+
+    }
+}
